95967ff814847503c15b13d0b7e4180ffc3cffab
Unknown <yotova.karina@gmail.com>
2020-09-22 12:53:26 +0300

    Added Chainblock homework.
    Added Chainblock homework.

---
+++ 22. Chainblock/Chainblock.Tests/TransactionTest.cs
@@ -0,0 +1,119 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Chainblock.Tests
+{
+    [TestFixture]
+    public class TransactionTest
+    {
+        [Test]
+        public void TestIfConstructorWorksCorrectly()
+        {
+            var id = 1;
+            TransactionStatus tr = TransactionStatus.Successfull;
+            var from = "TestSender";
+            var to = "TestReceiver";
+            var amount = 10.00;
+
+            var transcation = new Transaction(id, tr, from, to, amount);
+
+            Assert.AreEqual(transcation.Id, id);
+            Assert.AreEqual(tr, transcation.Status);
+            Assert.AreEqual(transcation.From, from);
+            Assert.AreEqual(transcation.To, to);
+            Assert.AreEqual(transcation.Amount, amount);
+        }
+        [Test]
+        public void NegativeIdShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(-1, tr, "TestFrom", "TestTo", 10), Throws.ArgumentException.With.Message.EqualTo("ID cannot be negative."));
+        }
+
+         [Test]
+        public void ZeroIdShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(0, tr, "TestFrom", "TestTo", 10), Throws.ArgumentException.With.Message.EqualTo("ID cannot be zero."));
+        }
+
+        [Test]
+        public void NullFromShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, null, "TestTo", 10), Throws.ArgumentException.With.Message.EqualTo("Sender cannot be null or empty."));
+        }
+
+        [Test]
+        public void WhiteSpaceFromShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, " ", "TestTo", 10), Throws.ArgumentException.With.Message.EqualTo("Sender cannot be null or empty."));
+        }
+
+        [Test]
+        public void EmptyFromShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, string.Empty, "TestTo", 10), Throws.ArgumentException.With.Message.EqualTo("Sender cannot be null or empty."));
+        }
+
+        [Test]
+        public void NullToShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, "TestSender", null, 10), Throws.ArgumentException.With.Message.EqualTo("Receiver cannot be null or empty."));
+        }
+
+        [Test]
+        public void WhiteSpaceToShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, "TestSender", " ", 10), Throws.ArgumentException.With.Message.EqualTo("Receiver cannot be null or empty."));
+        }
+
+        [Test]
+        public void EmptyToShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+           //Act//Assert
+            Assert.That(() => new Transaction(10, tr, "TestSender", string.Empty, 10), Throws.ArgumentException.With.Message.EqualTo("Receiver cannot be null or empty."));
+        }
+
+        [Test]
+        public void NegativeAmountShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, "TestFrom", "TestTo", -10), Throws.ArgumentException.With.Message.EqualTo("Amount cannot be less than zero."));
+        }
+
+        [Test]
+        public void ZeroAmountShouldThrowException()
+        {
+            //Arrange
+            TransactionStatus tr = TransactionStatus.Successfull;
+            //Act//Assert
+            Assert.That(() => new Transaction(10, tr, "TestFrom", "TestTo", 0), Throws.ArgumentException.With.Message.EqualTo("Amount cannot be zero."));
+        }
+    }
+}
---
+++ 22. Chainblock/Chainblock/Transaction.cs
@@ -0,0 +1,86 @@
+﻿using Chainblock.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chainblock
+{
+    public class Transaction : ITransaction
+    {
+        private int id;
+        private string from;
+        private string to;
+        private double amount;
+
+        public Transaction(int id, TransactionStatus transactionStatus, string from, string to, double amount)
+        {
+            this.Id = id;
+            this.Status = transactionStatus;
+            this.From = from;
+            this.To = to;
+            this.Amount = amount;
+        }
+        public int Id 
+        {
+            get => this.id;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("ID cannot be negative.");
+                }
+                if (value == 0)
+                {
+                    throw new ArgumentException("ID cannot be zero.");
+                }
+
+                this.id = value;
+            }
+        }
+        public TransactionStatus Status { get; set; }
+        public string From 
+        {
+            get => this.from;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value == " ")
+                {
+                    throw new ArgumentException("Sender cannot be null or empty.");
+                }
+
+                this.from = value;
+            }
+        }
+        public string To
+        {
+            get => this.to;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value == " ")
+                {
+                    throw new ArgumentException("Receiver cannot be null or empty.");
+                }
+
+                this.to = value;
+            }
+        }
+        public double Amount 
+        {
+            get => this.amount;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Amount cannot be less than zero.");
+                }
+
+                if (value == 0)
+                {
+                    throw new ArgumentException("Amount cannot be zero.");
+                }
+
+                this.amount = value;
+            } 
+        }
+    }
+}
